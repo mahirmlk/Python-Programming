0318884c032c518cf86e0bf77b6de8a47bf41ae6
mahir <mahirmalikx@gmail.com>
2025-10-26 12:15:13 +0530

    Use of print statement in python.

--- 07_comment_print_esc/print.py
+++ 07_comment_print_esc/print.py
@@ -1,45 +1,45 @@
 # Basic print examples
-print("Hello World")  # Simple string
-print(18)  # Number
-print(True)  # Boolean
-print(3.14)  # Float
+print("Hello World")  
+print(18) 
+print(True) 
+print(3.14)
 
 # Print multiple items
 print("Name:", "Mahir", "Age:", 25)
 
-# Using sep parameter (separator between items)
-print("Python", "is", "awesome", sep="-")  # Output: Python-is-awesome
-print("Date", "24", "08", "2025", sep="/")  # Output: Date/24/08/2025
-print("Hello", "World", sep="***")  # Output: Hello***World
-print( "Hello", "World", sep=" ")  # Output: Hello World
+# Using sep parameter
+print("Python", "is", "awesome", sep="-")  
+print("Date", "24", "08", "2025", sep="/")  
+print("Hello", "World", sep="***")  
+print( "Hello", "World", sep=" ") 
 
-# Using end parameter (what to print at the end)
-print("Hello", end="!")  # Output: Hello!
-print("World")  # Output: World (on new line)
+# Using end parameter 
+print("Hello", end="!") 
+print("World") 
 
 print("One", end=" ")
 print("Two", end=" ")
-print("Three")  # Output: One Two Three
+print("Three") 
 
 # Combining sep and end
 print("Python", "Java", "C++", sep=" | ", end="\n")
-print("programming languages ")  # Output: Python | Java | C++
+print("programming languages ")
 
 # Print with formatting
 name = "Mahir"
 age = 25
 print(f"Name: {name}, Age: {age}")  # f-string
-print("Name: {}, Age: {}".format(name, age))  # format method
+print("Name: {}, Age: {}".format(name, age)) 
 
 # Print empty lines
 print()  # Prints a blank line
 print("\n")  # Prints two blank lines
 
 # Print special characters
-print("This\tis\ttabbed")  # Using tab
-print("Line 1\nLine 2")  # Using newline
-print("He said, \"Hello!\"")  # Using double quotes inside string
-print('It\'s a beautiful day!')  # Using single quote inside string
+print("This\tis\ttabbed")  
+print("Line 1\nLine 2") 
+print("He said, \"Hello!\"") 
+print('It\'s a beautiful day!') 
 
 # Print with different number systems
 number = 42
