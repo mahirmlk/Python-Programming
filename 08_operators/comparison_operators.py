5a506386bdce140a32324f047410e491e73b8ac0
mahir <mahirmalikx@gmail.com>
2025-10-31 18:11:57 +0530

    Refactor comparison operators example by removing output comments for clarity

--- 08_operators/comparison_operators.py
+++ 08_operators/comparison_operators.py
@@ -2,23 +2,21 @@
 x = 10
 y = 20
 
-# Greater than (>) - returns True if left operand is greater
-print("x > y:", x > y)  # Output: False
+# Greater than (>) 
+print("x > y:", x > y)  
 
-# Less than (<) - returns True if left operand is smaller
-print("x < y:", x < y)  # Output: True
+# Less than (<) 
+print("x < y:", x < y)  
 
-# Equal to (==) - returns True if both operands are equal
-print("x == y:", x == y)  # Output: False
+# Equal to (==) 
+print("x == y:", x == y)  
 
-# Not equal to (!=) - returns True if operands are not equal
-print("x != y:", x != y)  # Output: True
+# Not equal to (!=) 
+print("x != y:", x != y)  # 
 
 # Greater than or equal to (>=)
-print("x >= y:", x >= y)  # Output: False
+print("x >= y:", x >= y)  
 
 # Less than or equal to (<=)
-print("x <= y:", x <= y)  # Output: True
+print("x <= y:", x <= y)  # 
 
-# Note: Comparison operators return boolean values (True or False)
-# commonly used in conditional statements and loops.
