dca7a6c8b714d59cf226f9cde7cbfbae216cf19b
mahir <mahirmalikx@gmail.com>
2025-11-01 17:48:28 +0530

    Remove unnecessary comments for clarity in if statement examples

--- 09_conditionals.py/if_statement.py
+++ 09_conditionals.py/if_statement.py
@@ -1,23 +1,24 @@
 # Basic of If Statement 
+
 # Simple condition check
 age = 20
 
-# If statement - executes only when condition is True
+# Single path of execution
 if age >= 18:
-    print("You are an adult")  # This will execute
-    print("You can vote")      # This will also execute
+    print("You are an adult")
+    print("You can vote")      # 
 
 # Another example with different condition
 temperature = 35
 
 if temperature > 30:
-    print("It's a hot day!")   # This will execute
+    print("It's a hot day!")
 
 # Multiple conditions can be in separate if statements
 score = 85
 
 if score >= 90:
-    print("Excellent!")       # This won't execute
+    print("Excellent!")     
 
 if score >= 80:
-    print("Good job!")        # This will execute
+    print("Good job!")     
