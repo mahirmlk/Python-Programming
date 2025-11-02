0dcb9c8226374ed816587d0af1731ef9cfad2c74
mahir <mahirmalikx@gmail.com>
2025-11-02 22:53:03 +0530

    Refactor string section headers and improve string concatenation example

--- 11_strings/01_strings.py
+++ 11_strings/01_strings.py
@@ -1,4 +1,4 @@
-# --- String Creation ---
+# String Creation
 
 # Strings can be created using single quotes, double quotes, or triple quotes.
 simple_string = 'Hello, World!'
@@ -6,31 +6,25 @@ another_string = "Python is fun."
 multi_line_string = """This is a multi-line string.
 It can span across several lines."""
 
-print("--- String Creation ---")
 print(simple_string)
 print(another_string)
 print(multi_line_string)
 print("-" * 20)
 
 
-# --- String Concatenation ---
+# String Concatenation
 
 # You can combine strings using the + operator.
 first_name = "KL"
 last_name = "Rahul"
-full_name = first_name + " " + last_name
-
-print("--- String Concatenation ---")
+full_name = " first_name " + " last_name "
 print(full_name)
 
-
-# --- String Formatting ---
-
-# f-strings (formatted string literals) are a modern and easy way to format strings.
+# String Formatting
 age = 30
 formatted_string = f"{full_name} is {age} years old."
 
-print("--- String Formatting ---")
+print( "String Formatting")
 print(formatted_string)
 print("-" * 20)
 
